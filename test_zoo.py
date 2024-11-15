2972f91fb260939279b7985f62348c0a869b248b
ppearslk <pearsaligupt@gmail.com>
2024-11-15 07:21:38 +0000

    Add the test cases

--- test_zoo.py
+++ test_zoo.py
@@ -4,10 +4,32 @@ from zoo import Zoo
 class TestZoo(unittest.TestCase):
     def setUp(self):
         self.zoo = Zoo()
-
+# Price_ticket 50 ------------------------------------------------------------ 
     def test_child_ticket_price(self):
-        self.assertEqual(self.zoo.get_ticket_price(5), 50)
-       
+        self.assertEqual(self.zoo.get_ticket_price(0), 50)
+    def test_child_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(12), 50)
+
+# Price_ticket 100 students ---------------------------------------------------
+    def test_students_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(13), 100)
+    def test_students_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(20), 100)
+
+# Price_ticket 150 -------------------------------------------------------------
+    def test_adults_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(21), 150)
+    def test_adults_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(60), 150)  
+
+# Price_ticket 100 seniors -----------------------------------------------------   
+    def test_seniors_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(61), 100)
+
+# Price invalid when age < 0 --------------------------------------------------
+    def test_invalid_ticket_price(self):
+        self.assertEqual(self.zoo.get_ticket_price(-1), "invalid")
+    
     # Add your additional test cases here.
 
 if __name__ == '__main__':
